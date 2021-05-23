1d5cf7e89510d58e33216cd20138ed5235aa670a
Sharfuzzaman Sajib <sharfuzzaman>
2021-05-23 16:34:26 +0600

    database password given

--- Trail-1/connect.php
+++ Trail-1/connect.php
@@ -1,6 +1,6 @@
 <?php
     //database connection
-	$conn = new mysqli('localhost', 'root', '', 'malltest');
+	$conn = new mysqli('localhost', 'sajib', 'zxcvbnm', 'malltest');
 	if($conn->connect_error){
 		die('Connection Failed : ' .$conn->connect_error);
 	}
